a019a5851db97574f11c626ca1b0606f3a52849b
Jonathon Grigg <jonathongrigg@gmail.com>
2012-10-05 13:52:35 +0800

    Artist info added to the top 100 list, prettier save file
    Unfortunately we have some interesting song titles (Gangnam style!) which
    throw out the adjustments for output, needs to be worked around for ljust()

--- musicanalyser.py
+++ musicanalyser.py
@@ -21,12 +21,28 @@ def getChartSource():
 	chartSourceEnd = pageSource.find('</div>', chartSourceBegin)
 	return pageSource[chartSourceBegin:chartSourceEnd]
 
-def findSongTitle(source, position):
+def getPositionString(source, position):
 	beginString = '<strong>' + str(position) + '.</strong>'
 	endString = '<strong>' + str(position + 1) + '.</strong>'
-	positionString = source[source.find(beginString):source.find(endString)]
-	titleEnd = positionString[:positionString.find('</a></h3><h4>')]
-	return titleEnd[titleEnd.rfind('">') + 2:]
+	return source[source.find(beginString):source.find(endString)]
+
+def findSongTitle(source, position):
+	positionString = getPositionString(source, position)
+	title = positionString[:positionString.find('</a></h3><h4>')]
+	return title[title.rfind('">') + 2:]
+
+def findSongArtist(source, position):
+ 	positionString = getPositionString(source, position)
+	artist = positionString[:positionString.find('</a></h4>')]
+	return artist[artist.rfind('">') + 2:]
+
+def sanitiseTitle(title):
+	# For the purpose of our output, we don't want the song name > 60 chars
+	# Blame the studios for "song name [rainbow unicorn version] (feat xyz)"
+	if len(title) > 60:
+		return title[:57] + '...'
+	else:
+		return title[:60]
 
 chartSource = getChartSource()
 characterFrequency = collections.defaultdict(int)
@@ -34,8 +50,9 @@ top100 = dict.fromkeys(range(1, 101))
 time = datetime.datetime.now().strftime('%Y-%m-%d')
 
 for i in range(1, 101):
-	title = findSongTitle(chartSource, i)
-	top100[i] = title
+	title = sanitiseTitle(findSongTitle(chartSource, i))
+	artist = findSongArtist(chartSource, i)
+	top100[i] = [title, artist]
 	characterFrequency[title.lower()[0]] += 1
 
 # Let's save this information to a dated file for future reference
@@ -43,8 +60,10 @@ saveFile = open('results-%s' % (time), "w")
 
 saveFile.write('Top 100 Songs for %s:\n' % (time))
 for position in top100:
-	print '%i: %s' % (position, top100[position])
-	saveFile.write('%i: %s \n' % (position, top100[position]))
+	print '%s: %s - %s' % \
+			(str(position).rjust(3), top100[position][0].ljust(60), top100[position][1])
+	saveFile.write('%s: %s - %s\n' % \
+			(str(position).rjust(3), top100[position][0].ljust(60),	top100[position][1]))
 
 saveFile.write('\nCharacter frequency:\n')
 for character in string.ascii_lowercase:
